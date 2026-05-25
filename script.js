ae725e16c7c62298246156119c43dfcb4b189262
Greyclaw <greyclawbox@gmail.com>
2026-05-25 20:07:04 +0300

    JS mobile view

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
+const navLinks = document.querySelectorAll('nav ul li a');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active');
+    });
+});
