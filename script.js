8aa8e86f6a7b6ba56580f0881d2a366de49516f5
Greyclaw <greyclawbox@gmail.com>
2026-05-25 20:25:05 +0300

    Added Observer

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('nav ul');
 const navLinks = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+const navItems = document.querySelectorAll('header nav ul li a');
+
+
 
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
@@ -10,4 +14,31 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         navMenu.classList.remove('active');
     });
+});
+
+const observerOptions = {
+    threshold: 0.2 
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        
+        if (entry.isIntersecting) {
+            
+            const currentId = entry.target.getAttribute('id');
+            
+            navItems.forEach(link => {
+                
+                link.classList.remove('active-link');
+                
+                if (link.getAttribute('href') === '#' + currentId) {
+                    link.classList.add('active-link');
+                }
+            });
+        }
+    });
+}, observerOptions);
+
+sections.forEach(section => {
+    observer.observe(section);
 });
